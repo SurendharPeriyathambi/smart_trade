ad284a22438a2943e47f83f55a387ca3890d84fe
Mukilan006 <mukiloffice@gmail.com>
2026-02-09 07:43:24 +0530

    mobile responseview correction

--- src/app/Components/sub-pages/footer/footer.ts
+++ src/app/Components/sub-pages/footer/footer.ts
@@ -9,14 +9,14 @@ import { Component } from '@angular/core';
 })
 export class Footer {
  links = {
-    company: ['Home', 'About Us', 'Courses', 'Pricing'],
+    company: ['Home', 'About Us', 'Courses'],
     support: ['Reviews', 'Contact', 'Terms of Service', 'Privacy Policy']
   };
 
   socialLinks = [
     { icon: 'bi bi-facebook', url: '#' },
     { icon: 'bi bi-twitter-x', url: '#' },
-    { icon: 'bi bi-vimeo', url: '#' },
+    // { icon: 'bi bi-vimeo', url: '#' },
     { icon: 'bi bi-youtube', url: '#' },
  
   ];
