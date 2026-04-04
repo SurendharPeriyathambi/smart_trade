bd77077a6a5a739faca55a692e78ab525e68316b
Mukilan <mukiloffice@gmail.com>
2026-04-04 11:16:09 +0530

    04-04 changes

--- src/app/Components/main-pages/home/home_service.ts
+++ src/app/Components/main-pages/home/home_service.ts
@@ -6,43 +6,43 @@ import { Banner, DemoVideos } from "../../../../interfaces/banner_interface";
 
 @Injectable({ providedIn: 'root' })
 export class HomeService {
-   
-     private bannerService = inject (BannerService);
+
+    private bannerService = inject(BannerService);
 
     //  Raw State Signals
-    protected banners = signal <Banner[]>([]);
-    private demoVideos =signal <DemoVideos[]>([]);
-    private loading = signal (false);
-    private error = signal <string | null> (null);
-    private activeVideoUrl = signal <string | null >(null);
+    protected banners = signal<Banner[]>([]);
+    private demoVideos = signal<DemoVideos[]>([]);
+    private loading = signal(false);
+    private error = signal<string | null>(null);
+    private activeVideoUrl = signal<string | null>(null);
     private _videoLoading = signal(false);
-    private _isModalOpen  = signal (false);
+    private _isModalOpen = signal(false);
     // public accesss for component
-   readonly banner = this.banners.asReadonly();
-  readonly demoVideo = this.demoVideos.asReadonly();
-  readonly loadings = this.loading.asReadonly();
-  readonly errors = this.error.asReadonly();
-  readonly activeVideoUrls = this.activeVideoUrl.asReadonly();
-  readonly videoLoading = this._videoLoading.asReadonly();
- readonly isModalOpen =  this._isModalOpen.asReadonly();
+    readonly banner = this.banners.asReadonly();
+    readonly demoVideo = this.demoVideos.asReadonly();
+    readonly loadings = this.loading.asReadonly();
+    readonly errors = this.error.asReadonly();
+    readonly activeVideoUrls = this.activeVideoUrl.asReadonly();
+    readonly videoLoading = this._videoLoading.asReadonly();
+    readonly isModalOpen = this._isModalOpen.asReadonly();
 
     // parent api calling..
-    loadHomeData ():void{
-         if (this.banners().length) return;
+    loadHomeData(): void {
+        if (this.banners().length) return;
         if (this.loading()) return;
         this.loading.set(true);
         this.error.set(null);
 
 
         this.bannerService.getHomeData().subscribe({
-            next:(res)=>{
+            next: (res) => {
                 if (res.status) {
-                    this.banners.set(res.data.banner??[]);
+                    this.banners.set(res.data.banner ?? []);
                     this.demoVideos.set(res.data.demo_videos ?? []);
                 }
                 this.loading.set(false);
             },
-            error:(err)=>{
+            error: (err) => {
                 this.error.set(err.message ?? "failed to load Data");
                 this.loading.set(false);
             }
@@ -50,25 +50,25 @@ export class HomeService {
 
     }
 
-    openVideo (videoId:string){
+    openVideo(videoId: string) {
         if (this._videoLoading()) return;
         this._videoLoading.set(true);
-        
+
         this.bannerService.getVideoUrl(videoId).subscribe({
-            next:(res)=>{
+            next: (res) => {
                 if (res.status) {
                     this.activeVideoUrl.set(res.data.cdn_url);
                     this._isModalOpen.set(true)
                 }
                 this._videoLoading.set(false);
             },
-            error:()=> this._videoLoading.set(false)
+            error: () => this._videoLoading.set(false)
         });
     }
 
-     closeVideo(): void {
-    this.activeVideoUrl.set(null);
-    this._isModalOpen.set(false);
-  }
+    closeVideo(): void {
+        this.activeVideoUrl.set(null);
+        this._isModalOpen.set(false);
+    }
 
 }
