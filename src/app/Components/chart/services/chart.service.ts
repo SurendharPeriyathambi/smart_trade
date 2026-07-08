763639fd2c9390980088c0aa0679f6739b71ca67
Mukilan006 <mukiloffice@gmail.com>
2026-07-08 13:39:12 +0530

    chart Service chnages

--- src/app/Components/chart/services/chart.service.ts
+++ src/app/Components/chart/services/chart.service.ts
@@ -1,20 +1,19 @@
 import { inject, Injectable } from "@angular/core";
-import { HttpEngine } from "../../../../services/engine/http_engine";
 import { Observable } from "rxjs";
 import { ApiResponce } from "../../../../interfaces/banner_interface";
-import { List, ListData } from "../model/chart.model";
-import { HttpClient, HttpParams ,HttpHeaders} from "@angular/common/http";
-import { environment } from "../../../environment";
+import { ListData } from "../model/chart.model";
+import { HttpClient, HttpParams } from "@angular/common/http";
+
 
 @Injectable({providedIn:'root'})
 export class ListService {
     private http = inject(HttpClient);
-    url=environment.chartUrl;
+    url="";
 
     getList():Observable<ApiResponce<ListData>>{
         const params = new HttpParams()
   .set('tag', 'active');
-        return this.http.get<ApiResponce<ListData>>(`${this.url}task/list`, 
+        return this.http.get<ApiResponce<ListData>>(`task/list`, 
     {
       params: params
     });  
